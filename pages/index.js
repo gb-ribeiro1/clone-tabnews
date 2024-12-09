b23e0f1cc20a56401020d1ab4e8e3a4ecc7bb884
Gabriel Ribeiro <89546926+gb-ribeiro1@users.noreply.github.com>
2024-12-09 04:16:27 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>pz, se vc me ama, compre uma baioneta no cs para mim</h1>
+    return <h1>pz, se vc me ama, compre uma baioneta no cs para mim! 😎</h1>
 }
 
 export default Home;
