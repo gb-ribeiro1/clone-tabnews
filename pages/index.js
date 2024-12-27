f1759ee2d3d8dbc407b2f6bc243f8625701620ff
Gabriel Ribeiro <89546926+gb-ribeiro1@users.noreply.github.com>
2024-12-27 00:07:04 +0000

    adiociona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>pz, se vc me ama, compre uma baioneta no cs para mim! 😎</h1>
+function Home() {
+  return <h1>pz, se vc me ama, compre uma baioneta no cs para mim! 😎</h1>;
+}
+
+function teste() {
+  console.log("indentação errda");
 }
 
 export default Home;
